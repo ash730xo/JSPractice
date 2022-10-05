4109bc33070334aa8b9c7a8ab52855195843e5de
ash730xo <109447309+ash730xo@users.noreply.github.com>
2022-10-05 13:55:52 -0400

    Add files via upload
    Comments

--- practice.js
+++ practice.js
@@ -9,18 +9,28 @@ functions:
     double it and put it in input 2
  */
 
+// Gets id from HTML code 
 let textbox = document.getElementById("text");
-
 let button = document.getElementById("button");
 
-// On click listener
+// On click listener-- handles everything once the user clicks the submit button
 button.onclick = function () {
+    // gets clients input
     textInput = textbox.value;
     textInput.split('')
+
+    //variable place holder
     let textOutput = ""; 
 
+
+    // For loop that iterates over user input from textbox #1 
+    // and puts it in a place holder
     for(let i = 0; i < textInput.length; i++){
+        //iterates through user input and at each index
+        //it will double it and add it to the place holder
        textOutput += (textInput[i] + textInput[i] );
+
+       //Output to textbox #2
        document.getElementById("text1").value = textOutput
     }
 
